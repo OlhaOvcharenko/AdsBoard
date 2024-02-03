ad3a405bb6855523596c48b1f4a9e90ccb0c97da
unknown <ovcharenko.olga.2014@gmail.com>
2024-02-03 12:29:16 +0300

    change file All posts by adding displaying images and displaying ads by alphabet

--- client/src/components/features/AllPosts/AllPosts.js
+++ client/src/components/features/AllPosts/AllPosts.js
@@ -6,44 +6,51 @@ import { Spinner } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { loadAdsRequest } from "../../../redux/adsRedux";
 import { useEffect } from "react";
+import { Button } from "react-bootstrap";
+import { IMAGES_URL } from "../../../config";
 
 const AllAds = () => {
-    const dispatch = useDispatch();
-    const ads = useSelector(getAllAds);
-    const request = useSelector(state => getRequest(state, LOAD_ADS));
-    console.log(request, 'requests');
-  
-    useEffect(() => {
-      dispatch(loadAdsRequest());
-    }, [dispatch]);
-    
-    if (!request || !request.success) {
-      return <Spinner color="primary" className="standard-box d-block me-auto ms-auto" />;
-    } else {
-      return (
-        <div>
-          <Stack direction="horizontal" gap={4}>
-            <div className="p-2">
-              <h1>All ads</h1>
-            </div>
-          </Stack>
-          <div className="row my-5">
-            {ads.map((ad) => (
-              <Col key={ad.id} className="col-lg-4 col-sm-12 col-md-6 px-2 py-2">
-                <Card>
-                  <Card.Body>
-                    <Card.Title>{ad.title}</Card.Title>
-                    <Card.Text className="mb-2">
-                      <b>Author:</b> {ad.author.login}
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
-          </div>
+  const dispatch = useDispatch();
+  const ads = useSelector(getAllAds);
+  const request = useSelector(state => getRequest(state, LOAD_ADS));
+  console.log(request, 'requests');
+  console.log(ads, 'ads');
+
+  useEffect(() => {
+    dispatch(loadAdsRequest());
+  }, [dispatch]);
+
+  const sortedAds = ads.slice().sort((a, b) => a.title.localeCompare(b.title));
+
+  if (!request || !request.success) {
+    return <Spinner color="primary" className="standard-box d-block me-auto ms-auto" />;
+  } else {
+    return (
+    <div>
+        <Stack direction="horizontal" gap={4}>
+        <div className="p-2">
+            <h1>ADVERTISMENTS</h1>
         </div>
-      );
-    }
-  };
+        </Stack>
+        <div className="row my-5">
+        {sortedAds.map((ad) => (
+          <Col key={ad.id} className="col-lg-4 col-sm-12 col-md-6 px-2 py-2">
+             <Card>
+              <Card.Body>
+                <Card.Img variant="top" src={`${IMAGES_URL}/${ad.photo}`} style={{ height: '20rem', objectFit: 'cover' }} />
+                <Card.Title>{ad.title}</Card.Title>
+                <Card.Text className="mb-2">
+                    <b>Location:</b> {ad.location}
+                </Card.Text>
+                <Button variant="primary">Read More</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+        </div>
+    </div>
+    );
+  }
+};
   
   export default AllAds;
