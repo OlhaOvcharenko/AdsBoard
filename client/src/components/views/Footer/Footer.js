704ebdd8aad66040e4b754a8193bbd689e1c5f07
unknown <ovcharenko.olga.2014@gmail.com>
2024-02-03 12:32:10 +0300

    add style for footer

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -1,6 +1,8 @@
 const Footer = () => (
     <div>
-       Footer
+     <footer className='text-center text-muted py-4'>
+        <p>Copyright © AdsBoard 2024</p>
+      </footer>
     </div>
   );
   
