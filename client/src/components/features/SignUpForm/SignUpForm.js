5417bd85eca28cf288063b7c4776acb08d66b5a8
unknown <ovcharenko.olga.2014@gmail.com>
2024-02-05 22:23:55 +0300

    change form for registration by adding fetching data

--- client/src/components/features/SignUpForm/SignUpForm.js
+++ client/src/components/features/SignUpForm/SignUpForm.js
@@ -1,34 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 import  { Button } from "react-bootstrap";
+import { API_URL } from "../../../config";
+import { Alert, Spinner } from "react-bootstrap";
 
 const SignUpForm = () => {
 
+  const [ login, setLogin] = useState();
+  const [ password, setPassword] = useState();
+  const [ phoneNumber, setPhoneNumber] = useState();
+  const [ avatar, setAvatar]= useState(null)
+  const [status, setStatus] = useState(null); // null, 'loading', 'success', 'serverError', 'clientError', 'loginError'
+
+  const handleSubmit =(e) => {
+    e.preventDefault();
+
+    const fd = new FormData();
+    fd.append('login', login);
+    fd.append('password', password);
+    fd.append('phoneNumber', phoneNumber);
+    fd.append('avatar', avatar);
+
+    const options = {
+      method: 'POST',
+      body: fd
+    }
+
+    setStatus("loading");
+    fetch(`${API_URL}/auth/register`, options)
+      .then((res) => {
+        if (res.status === 201) {
+          setStatus("success");
+        } else if (res.status === 400) {
+          setStatus("clientError");
+        } else if (res.status === 409) {
+          setStatus("loginError");
+        } else {
+          setStatus("serverError");
+        }
+      })
+      .catch((err) => {
+        setStatus("serverError");
+      });
+  }
 
   return (
     <div>
-      <Form className="col-12 col-sm-4 mx-auto">
+      <Form className="col-12 col-sm-4 mx-auto" onSubmit={handleSubmit}>
 
         <h1 className="my-4">Sign Up!</h1>
 
+        {status === "success" && (
+        <Alert variant="success">
+          <Alert.Heading>Success!</Alert.Heading>
+          <p>You have successfully registered. Now you can log in...</p>
+        </Alert> )}
+        {status === "serverError" && (
+        <Alert variant="danger">
+          <Alert.Heading>Something  went wrong...</Alert.Heading>
+          <p>Unexpected error... Try again!</p>
+        </Alert>)}
+        {status === "clientError" && (
+        <Alert variant="danger">
+          <Alert.Heading>Not enought data.</Alert.Heading>
+          <p>You have to fill all the fileds.</p>
+        </Alert>)}
+        {status === "loginError" && (
+        <Alert variant="warning">
+          <Alert.Heading>Login already in use.</Alert.Heading>
+          <p>You have to use another login.</p>
+        </Alert>)}
+        
+        {status === "loading" && (
+        <Spinner color="primary" className="standard-box d-block me-auto ms-auto" />
+        )}
+
         <Form.Group className="mb-3" controlId="formLogin">
           <Form.Label>Login</Form.Label>
-          <Form.Control type="text" placeholder="Enter login"/>
+          <Form.Control type="text" value={login} onChange={e => setLogin(e.target.value)} placeholder="Enter login"/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password"/>
+          <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password"/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formPhoneNumber">
           <Form.Label>Phone Number</Form.Label>
-          <Form.Control type="tel" placeholder="Phone number"/>
+          <Form.Control type="tel" value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)} placeholder="Phone number"/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formFile">
           <Form.Label>Avatar</Form.Label>
-          <Form.Control type="file"/>
+          <Form.Control type="file" onChange={e => setAvatar(e.target.files[0])}/>
         </Form.Group>
 
         <Button variant="primary" type="submit" className="my-3">
