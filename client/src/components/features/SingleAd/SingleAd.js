a175f56b2db1505d105212a83eb55ee9ca0595f4
unknown <ovcharenko.olga.2014@gmail.com>
2024-02-11 13:33:53 +0300

    change single add by adding condition for rendering of element if user is null

--- client/src/components/features/SingleAd/SingleAd.js
+++ client/src/components/features/SingleAd/SingleAd.js
@@ -23,68 +23,63 @@ const SingleAd = (props) => {
 
   const adData = useSelector(state => getAdById(state, id));
   const user = useSelector(getUser);
-  const userAvatar = user.currentUser.user.avatar;
+
   const authorImg = adData.author.avatar;
   console.log(authorImg)
-  console.log(userAvatar);
+
 
   const handleDelete = () => {
     dispatch(deleteAdsRequest(id))
     navigate('/'); 
   };
 
-  const authorOfAd = user.currentUser && user.currentUser.user._id === adData.author._id;
+  const authorOfAd = user?.currentUser?.user._id === adData.author._id;
 
-  if (!adData) return <Navigate to="/" />;
-  else {
-    
-    return (
-      <Row className="justify-content-center">
-        <Col className="col-lg-6">
-          <Row>
-            <Col>
-              <h2 className={styles.title}>{adData.title}</h2>
-            </Col>
-          </Row>
-          <Card className="border-0">
-            <Card.Body>
-             <Card.Text className="m-3 d-flex align-items-center">
-                <FontAwesomeIcon icon={faMapLocation} className="mr-2 px-2" />
-                  {adData.location}
-              </Card.Text>
+  if (!adData) navigate('/')
+  else 
+  return (
+    <Row className="justify-content-center">
+      <Col className="col-lg-6">
+        <Row>
+          <Col>
+            <h2 className={styles.title}>{adData.title}</h2>
+          </Col>
+        </Row>
+        <Card className="border-0">
+          <Card.Body>
+            <Card.Text className="m-3 d-flex align-items-center">
+              <FontAwesomeIcon icon={faMapLocation} className="mr-2 px-2" />
+              {adData.location}
+            </Card.Text>
 
-              <Card.Img variant="top m-2" src={`${IMAGES_URL}/${adData.photo}`} style={{ height: '20rem', objectFit: 'cover' }} />
-              <Card.Text className="m-3" dangerouslySetInnerHTML={{ __html: adData.description }}></Card.Text>
-              
-              <Card.Text className="m-3">
-                <FontAwesomeIcon icon={faMoneyBill} className="mr-2 px-2" /> {adData.price} PLN
-              </Card.Text>
-
+            <Card.Img variant="top m-2" src={`${IMAGES_URL}/${adData.photo}`} style={{ height: '20rem', objectFit: 'cover' }} />
+            <Card.Text className="m-3" dangerouslySetInnerHTML={{ __html: adData.description }}></Card.Text>
             
-              <Card.Text className="mx-3 py-3 d-flex justify-content-between align-items-center">
-                <div className="d-flex align-items-center">
-                  <img src={`${IMAGES_URL}/${authorImg}`} alt="Avatar" style={{ width: '30px', height: '30px', borderRadius: '50%', marginRight: '10px' }} />
-                  <i>{adData.author.login}</i>
-                </div>
-                <p><i>Published Date: {adData.date}</i></p> {/* Updated */}
-              </Card.Text>
-                          
-              {authorOfAd && (
-                <Button as={Link} key={props.id} to={`/ads/edit/${adData._id}`} className="mt-2 mx-4" variant="secondary"> Edit advert </Button>
-              )}
+            <Card.Text className="m-3">
+              <FontAwesomeIcon icon={faMoneyBill} className="mr-2 px-2" /> {adData.price} PLN
+            </Card.Text>
 
-              {/* Delete button */}
-              {authorOfAd && (
-                <Button onClick={handleDelete} className="mt-2" variant="danger"> Delete advert </Button>
-              )}
+            <Card.Text className="mx-3 py-3 d-flex justify-content-between align-items-center">
+              <div className="d-flex align-items-center">
+                <img src={`${IMAGES_URL}/${authorImg}`} alt="Avatar" style={{ width: '30px', height: '30px', borderRadius: '50%', marginRight: '10px' }} />
+                <i>{adData.author.login}</i>
+              </div>
+              <p className="mt-2"><i>Published Date: {adData.date}</i></p> {/* Updated */}
+            </Card.Text>
+                        
+            {authorOfAd && (
+              <Button as={Link} key={props.id} to={`/ads/edit/${adData._id}`} className="mt-2 mx-4" variant="secondary"> Edit advert </Button>
+            )}
 
-             
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    );
-  }
+            {/* Delete button */}
+            {authorOfAd && (
+              <Button onClick={handleDelete} className="mt-2" variant="danger"> Delete advert </Button>
+            )}
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  );
 };
 
 export default SingleAd;
